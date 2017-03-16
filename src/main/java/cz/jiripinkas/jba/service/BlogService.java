a2301261587dacc86d4e79096080dea14df3a2a4
nirmalshrm119 <nirmalshrm119@gmail.com>
2017-03-16 23:39:12 +0530

    34th commit : UTF-8 Filter PreAuthorize

--- src/main/java/cz/jiripinkas/jba/service/BlogService.java
+++ src/main/java/cz/jiripinkas/jba/service/BlogService.java
@@ -1,6 +1,8 @@
 package cz.jiripinkas.jba.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.method.P;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Service;
 
 import cz.jiripinkas.jba.entity.Blog;
@@ -22,9 +24,13 @@ public class BlogService {
 		blog.setUser(user);
 		blogRepository.save(blog);
 	}
+	@PreAuthorize("#blog.user.name == authentication.name or hasRole('ROLE_ADMIN')")
+	public void delete(@P("blog") Blog blog) {
+		blogRepository.delete(blog);
+	}
 
-	public void delete(int id) {
-		blogRepository.delete(id);
+	public Blog findOne(int id) {
+		return blogRepository.findOne(id);
 	}
 	
 }
