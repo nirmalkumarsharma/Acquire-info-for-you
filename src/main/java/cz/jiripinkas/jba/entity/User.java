229baa446e35e1169af3f203a1b952e3b72edf12
nirmalshrm119 <nirmalshrm119@gmail.com>
2017-03-18 20:17:55 +0530

    40th commit : Unique Constraint validator implemetation

--- src/main/java/cz/jiripinkas/jba/entity/User.java
+++ src/main/java/cz/jiripinkas/jba/entity/User.java
@@ -3,6 +3,7 @@ package cz.jiripinkas.jba.entity;
 import java.util.List;
 
 import javax.persistence.CascadeType;
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
@@ -13,6 +14,8 @@ import javax.validation.constraints.Size;
 
 import org.hibernate.validator.constraints.Email;
 
+import cz.jiripinkas.jba.annotation.UniqueUsername;
+
 @Entity
 public class User {
 
@@ -21,6 +24,8 @@ public class User {
 		private Integer id;
 		
 		@Size(min = 3, message = "Name must be at least 3 characters!")
+		@Column(unique=true)
+		@UniqueUsername(message="Username already exist!")
 		private String name;
 		
 		@Email(message = "Invalid email address!")
